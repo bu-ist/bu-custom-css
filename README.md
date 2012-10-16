ebcd3c679cb9fa6259d310b67bb10ffcdd06e5ba
inderpreet99 <inderpreet99@gmail.com>
2012-10-16 12:51:14 -0400

    bump version 1.0.1

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 **Tags:** custom, css  
 **Requires at least:** 2.9  
 **Tested up to:** 3.1  
-**Stable tag:** 1.0  
+**Stable tag:** 1.0.1  
 
 Enables editing of a custom CSS file, with an option to override the original theme CSS.
 
--- bu-custom-css.php
+++ bu-custom-css.php
@@ -4,7 +4,7 @@ Plugin Name: BU Custom CSS Editor
 Description: Allows CSS editing with an option to override the original theme CSS.
 Author: Boston University (IS&T)
 Author URI: http://www.bu.edu/tech/
-Version: 1.0
+Version: 1.0.1
 */
 
 /**
